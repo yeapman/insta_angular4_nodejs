af7e493c7e4affcd7c62ea4913de15f1ce0565b4
DESKTOP-EB3BLCO\Admin <yeapman@ukr.net>
2017-05-16 17:13:15 +0300

    add working mongoose save

--- server.js
+++ server.js
@@ -6,15 +6,35 @@ var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 var url = 'mongodb://localhost:27017/databasePlumbum';
-var mongoclient = require('mongodb').MongoClient,
-  assert = require('assert');
 var UserController = require('./server/UserController');
 mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log('we are connected!');
+});
+var kotyaraSchema = mongoose.Schema({
+  name: String
+}, { collection: 'movie' });
+var kotyara = mongoose.model('Darcik', kotyaraSchema );
+
+var Imya = new kotyara({ name: 'Darsiiiii'});
+
+Imya.save(function(err, data) {
+  if (err) return console.log(err);
+  else console.log('Saved: ', data);
+});
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/users', UserController);
+
+
+
 
 var port = process.env.PORT || '3000';
 app.set('port', port);
